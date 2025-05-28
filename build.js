cfa8ddecef6d490b266bdabaae3cdf77e1014bc6
SebastianBenjamin <benjaminsebastian156@gmail.com>
2025-05-28 13:40:06 +0530

    Version Final

---
+++ build.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const firebaseConfig = `const firebaseConfig = ${JSON.stringify({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+}, null, 2)};`;
+
+fs.writeFileSync('public/firebase-config.js', firebaseConfig);
